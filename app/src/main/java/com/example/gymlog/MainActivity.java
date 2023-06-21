274d19e8ae579cade86d217d2e50787418b693df
lutzzdias <thiagolutzdias@hotmail.com>
2023-06-21 18:40:59 -0400

    feat: create current workout static UI

--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.gymlog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ActiveWorkout.class);
+            startActivity(intent);
+        });
     }
 }
