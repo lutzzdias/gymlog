eb6235392882f22aa903c97e260aba75bbe0c767
lutzzdias <thiagolutzdias@hotmail.com>
2023-06-22 16:45:30 -0400

    feat: create get exercises of a given workout method

--- app/src/main/java/com/example/gymlog/dao/WorkoutDao.java
+++ app/src/main/java/com/example/gymlog/dao/WorkoutDao.java
@@ -5,6 +5,7 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
+import com.example.gymlog.entities.Exercise;
 import com.example.gymlog.entities.Workout;
 
 import java.util.List;
@@ -17,8 +18,8 @@ public interface WorkoutDao {
     @Query("SELECT * FROM workouts WHERE id = :workoutId")
     Workout getById(int workoutId);
 
-    @Query("SELECT * FROM workouts WHERE id = :workoutId")
-    Workout getWorkoutExercises(int workoutId);
+    @Query("SELECT exercises FROM workouts WHERE id = :workoutId")
+    List<Exercise> getWorkoutExercises(int workoutId);
 
     @Insert
     void insert(Workout workout);
