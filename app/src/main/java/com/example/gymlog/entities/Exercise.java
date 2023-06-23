edc51bd027afd551e0c9b4787bbf4b9d7fddcaf5
lutzzdias <thiagolutzdias@hotmail.com>
2023-06-23 19:35:22 -0400

    feat: define id as autoGenerate and create constructor

--- app/src/main/java/com/example/gymlog/entities/Exercise.java
+++ app/src/main/java/com/example/gymlog/entities/Exercise.java
@@ -2,13 +2,19 @@ package com.example.gymlog.entities;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "exercises")
 public class Exercise {
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     public int id;
     public String name;
     @ColumnInfo(name="muscle_group")
     public String muscleGroup;
+
+    public Exercise(String name, String muscleGroup){
+        this.name = name;
+        this.muscleGroup = muscleGroup;
+    }
 }
