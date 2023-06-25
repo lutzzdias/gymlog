eef089da335219ea7b4aca22c089fe91a847a53b
lutzzdias <thiagolutzdias@hotmail.com>
2023-06-25 16:18:19 -0400

    refactor: change id type to string

--- app/src/main/java/com/example/gymlog/entities/Exercise.java
+++ app/src/main/java/com/example/gymlog/entities/Exercise.java
@@ -1,19 +1,28 @@
 package com.example.gymlog.entities;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
+import java.util.UUID;
+
 @Entity(tableName = "exercises")
 public class Exercise {
-    @PrimaryKey(autoGenerate = true)
-    public int id;
+    @PrimaryKey @NonNull
+    public String id;
     public String name;
     @ColumnInfo(name="muscle_group")
     public String muscleGroup;
 
-    public Exercise(String name, String muscleGroup){
+    @Ignore
+    public Exercise(String name, String muscleGroup) {
+        this(UUID.randomUUID().toString(), name, muscleGroup);
+    }
+
+    public Exercise(String id, String name, String muscleGroup){
+        this.id = id;
         this.name = name;
         this.muscleGroup = muscleGroup;
     }
