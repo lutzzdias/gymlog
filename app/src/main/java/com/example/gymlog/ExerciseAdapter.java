3fcaa1d171d30337a0e44cc5d5592412c5775ea9
CaueMaldonadoLima <caue.maldonado@ufms.br>
2023-06-25 21:33:53 -0400

    feat: add exercise adapter to code

---
+++ app/src/main/java/com/example/gymlog/ExerciseAdapter.java
@@ -0,0 +1,47 @@
+package com.example.gymlog;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class ExerciseAdapter extends ArrayAdapter<String> {
+    private List<String> dataList;
+    private LayoutInflater inflater;
+
+    public ExerciseAdapter(@NonNull Context context, List<String> dataList) {
+        super(context, 0, dataList);
+        this.dataList = dataList;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.exercise_layout, parent, false);
+            holder = new ViewHolder();
+            holder.textView = convertView.findViewById(R.id.txtWorkout);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        String data = dataList.get(position);
+        holder.textView.setText(data);
+
+        return convertView;
+    }
+    private static class ViewHolder {
+        TextView textView;
+    }
+}
--- app/src/main/java/com/example/gymlog/entities/Exercise.java
+++ app/src/main/java/com/example/gymlog/entities/Exercise.java
@@ -1,20 +1,20 @@
-package com.example.gymlog.entities;
+    package com.example.gymlog.entities;
 
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.Ignore;
-import androidx.room.PrimaryKey;
+    import androidx.room.ColumnInfo;
+    import androidx.room.Entity;
+    import androidx.room.Ignore;
+    import androidx.room.PrimaryKey;
 
-@Entity(tableName = "exercises")
-public class Exercise {
-    @PrimaryKey(autoGenerate = true)
-    public int id;
-    public String name;
-    @ColumnInfo(name="muscle_group")
-    public String muscleGroup;
+    @Entity(tableName = "exercises")
+    public class Exercise {
+        @PrimaryKey(autoGenerate = true)
+        public int id;
+        public String name;
+        @ColumnInfo(name="muscle_group")
+        public String muscleGroup;
 
-    public Exercise(String name, String muscleGroup){
-        this.name = name;
-        this.muscleGroup = muscleGroup;
+        public Exercise(String name, String muscleGroup){
+            this.name = name;
+            this.muscleGroup = muscleGroup;
+        }
     }
-}
