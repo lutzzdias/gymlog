4280aff00932b271cb995594d95c75639c31510a
Murilo Matias <m.matias@ufms.br>
2023-06-25 23:02:44 -0400

    feat: implements viewModel in createExercise

--- app/src/main/java/com/example/gymlog/CreateExercise.java
+++ app/src/main/java/com/example/gymlog/CreateExercise.java
@@ -1,26 +1,36 @@
 package com.example.gymlog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.example.gymlog.entities.Exercise;
+import com.example.gymlog.viewmodels.ExerciseViewModel;
 
 public class CreateExercise extends AppCompatActivity {
 
+    private ExerciseViewModel exerciseViewModel;
+    private EditText exerciseName;
+    private EditText exerciseGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_exercise);
-
-
+        exerciseName = findViewById(R.id.edtAWTitle);
+        exerciseGroup = findViewById(R.id.edtAWGroup);
+        exerciseViewModel = new ViewModelProvider(this).get(ExerciseViewModel.class);
         //ImageButton
         ImageButton iBClose = findViewById(R.id.iBttClose);
         iBClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 finish();
             }
         });
@@ -30,10 +40,10 @@ public class CreateExercise extends AppCompatActivity {
         bCreateExercise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Criar a logica de salvar no banco
-
-
-                finish();
+                String name = exerciseName.getText().toString();
+                String muscleGroup = exerciseGroup.getText().toString();
+                Exercise exercise = new Exercise(name, muscleGroup);
+                exerciseViewModel.insert(exercise);
             }
         });
     }
