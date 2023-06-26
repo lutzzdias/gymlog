a639df3325bd07d5fbe2a4c42c71274e6dbd665d
lutzzdias <thiagolutzdias@hotmail.com>
2023-06-25 22:42:42 -0400

    fix: remove conflict markers left in files

--- app/src/main/java/com/example/gymlog/ExerciseAdapter.java
+++ app/src/main/java/com/example/gymlog/ExerciseAdapter.java
@@ -10,43 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-<<<<<<< HEAD
-import java.util.List;
-
-public class ExerciseAdapter extends ArrayAdapter<String> {
-    private List<String> dataList;
-    private LayoutInflater inflater;
-
-    public ExerciseAdapter(@NonNull Context context, List<String> dataList) {
-        super(context, 0, dataList);
-        this.dataList = dataList;
-        inflater = LayoutInflater.from(context);
-    }
-
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        ViewHolder holder;
-
-        if (convertView == null) {
-            convertView = inflater.inflate(R.layout.exercise_layout, parent, false);
-            holder = new ViewHolder();
-            holder.textView = convertView.findViewById(R.id.txtWorkout);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-        String data = dataList.get(position);
-        holder.textView.setText(data);
-
-        return convertView;
-    }
-    private static class ViewHolder {
-        TextView textView;
-=======
 import com.example.gymlog.entities.Exercise;
-import com.example.gymlog.entities.Workout;
 
 import java.util.ArrayList;
 
@@ -66,12 +30,11 @@ public class ExerciseAdapter extends ArrayAdapter<Exercise> {
         Exercise exercisePosition = getItem(position);
 
         TextView exerciseName = currentItemView.findViewById(R.id.WEName);
-        exerciseName.setText(exercisePosition.getName());
+        exerciseName.setText(exercisePosition.name);
 
         TextView muscleGroup = currentItemView.findViewById(R.id.WEMuscleGroup);
-        muscleGroup.setText(exercisePosition.getMuscleGroup());
+        muscleGroup.setText(exercisePosition.muscleGroup);
 
         return currentItemView;
->>>>>>> 6e21de853a6d66e061d9863505ac7a325f143392
     }
 }
